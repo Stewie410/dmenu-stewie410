f9ae261d8cc113146fd6b7f310e624d6e198e28f
Stewie410 <stewie410@gmail.com>
2020-08-16 15:36:54 -0400

    Removed fuzzy-highlight 'drawhighlights' definition

--- dmenu.c
+++ dmenu.c
@@ -120,45 +120,6 @@ cistrstr(const char *s, const char *sub)
 static void
 drawhighlights(struct item *item, int x, int y, int maxw)
 {
-	int i, indent;
-	char *highlight;
-	char c;
-
-	if (!(strlen(item->text) && strlen(text)))
-		return;
-
-	drw_setscheme(drw, scheme[item == sel
-	                   ? SchemeSelHighlight
-	                   : SchemeNormHighlight]);
-	for (i = 0, highlight = item->text; *highlight && text[i];) {
-		if (*highlight == text[i]) {
-			/* get indentation */
-			c = *highlight;
-			*highlight = '\0';
-			indent = TEXTW(item->text);
-			*highlight = c;
-
-			/* highlight character */
-			c = highlight[1];
-			highlight[1] = '\0';
-			drw_text(
-				drw,
-				x + indent - (lrpad / 2),
-				y,
-				MIN(maxw - indent, TEXTW(highlight) - lrpad),
-				bh, 0, highlight, 0
-			);
-			highlight[1] = c;
-			i++;
-		}
-		highlight++;
-	}
-}
-
-
-static void
-drawhighlights(struct item *item, int x, int y, int maxw)
-{
 	char restorechar, tokens[sizeof text], *highlight,  *token;
 	int indentx, highlightlen;
 
